0450788b1b7e42c1ea4fc5996a8cb8261cc822c8
bbre1666 <badenbrenner@isu.edu>
2023-01-16 14:42:25 -0700

    Add project files.

---
+++ SayMyName/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+Imports System.Net.Security
+
+Module Program
+    Sub Main(args As String())
+        'Console.WriteLine("Hello World!")
+        Console.WriteLine("HelloWhatIsyourName")
+        Console.ReadLine("")
+        Console.WriteLine("name")
+
+
+
+    End Sub
+End Module
