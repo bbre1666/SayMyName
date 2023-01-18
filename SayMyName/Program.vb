376cc4e8fc1dde0138fd3e63e6c49fa7f4a0a906
bbre1666 <badenbrenner@isu.edu>
2023-01-18 08:17:24 -0700

    format

--- SayMyName/Program.vb
+++ SayMyName/Program.vb
@@ -1,16 +1,21 @@
 Imports System
 Imports System.Net.Security
+'Baden Brenner
+'RCET0265
+'Spring 2023
+'Say My Name
+'https://github.com/bbre1666/SayMyName.git
 
 Module Program
     Sub Main(args As String())
-        'Console.WriteLine("Hello World!")
+
         Console.WriteLine("Hello What Is your Name")
+
         Dim name As String
+
         name = Console.ReadLine()
 
         Console.WriteLine("hello" & " " & name)
-
-
 
     End Sub
 End Module
